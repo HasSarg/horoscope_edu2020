b47ba1ece7291e5e586efa1aeed244d997d23d6d
hasmik_s2 <hassarg76@mail.ru>
2020-09-08 12:56:53 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include "person.h"
+
+int main()
+{
+  unsigned int n;
+  std::cin >> n;
+  std::string name;
+  std::string last_name;
+  unsigned int year;
+  unsigned int month;
+  unsigned int day;
+  std::vector<Person> persons;
+  for(int i = 0; i < n; ++i) {
+    std::cin >> name >> last_name >> year >> month >> day;
+    persons.push_back(Person{name, last_name, year, month, day});
+  }
+  for(int i = 0; i < n; ++i) {
+    persons[i].print();
+  }
+  return 0;
+}
---
+++ person.cpp
@@ -0,0 +1,110 @@
+#include "person.h"
+
+Person::Person(std::string name, std::string last_name, unsigned int year,
+               unsigned int  month, unsigned int day)
+               : m_name(name)
+               , m_last_name(last_name)
+               , m_year(year)
+               , m_month(month)
+               , m_day(day)
+{
+    check_set_year();
+    check_set_month();
+    check_set_day();
+    set_zodiac_sign_year();
+    set_zodiac_sign_month();
+}
+
+void Person::print() const
+{
+  std::cout << m_name << " " << m_last_name << " "  << m_zodiac_sign_year
+            << " " << m_zodiac_sign_month << std::endl;
+}
+
+void Person::set_max_days()
+{
+  switch(m_month) {
+    case 4: case 6: case 9: case 11:
+      m_max_days = 30;
+      break;
+    case 2:
+      if(m_is_leap_year) {
+        m_max_days = 29;
+      }else{
+        m_max_days = 28;
+      }
+      break;
+    default:
+      m_max_days = 31;
+  }
+}
+
+void Person::check_set_year()
+{
+  if(m_year < MIN_YEAR) {
+    m_year = MIN_YEAR;
+  } else if(m_year > MAX_YEAR) {
+    m_year = MAX_YEAR;
+  }
+  if ((m_year % 4 == 0 && m_year % 100 != 0) || m_year % 400 == 0) {
+    m_is_leap_year = true;
+  }
+}
+
+void Person::check_set_month()
+{
+  if(m_month < MIN_MONTH) {
+    m_month = MIN_MONTH;
+  } else if(m_month > MAX_MONTH) {
+    m_month = MAX_MONTH;
+  }
+}
+
+void Person::check_set_day()
+{
+  set_max_days();
+  if(m_day < 1) {
+    m_day = 1;
+  } else if (m_day > m_max_days) {
+    m_day = m_max_days;
+  }
+}
+
+
+void Person::set_zodiac_sign_year()
+{
+  std::string zodiac_year_signs[12] = {"Monkey", "Rooster", "Dog", "Pig",
+                                       "Rat", "Ox", "Tiger", "Rabbit",
+                                       "Dragon", "Snake", "Horse", "Sheep"};
+  unsigned int rem = m_year % 12;
+  m_zodiac_sign_year = zodiac_year_signs[rem];
+}
+
+void Person::set_zodiac_sign_month()
+{
+  if((m_month == 12 and m_day > 23) or (m_month == 1 and m_day < 21)) {
+    m_zodiac_sign_month = "Capricorn";
+  } else if((m_month == 1 and m_day > 22) or (m_month == 2 and m_day < 19)) {
+    m_zodiac_sign_month = "Aquarius";
+  } else if((m_month == 2 and m_day > 20) or (m_month == 3 and m_day < 21)) {
+    m_zodiac_sign_month = "Pisces";
+  } else if((m_month == 3 and m_day > 22) or (m_month == 4 and m_day < 21)) {
+    m_zodiac_sign_month = "Aries";
+  } else if((m_month == 4 and m_day > 22) or (m_month == 5 and m_day < 22)) {
+    m_zodiac_sign_month = "Taurus";
+  } else if((m_month == 5 and m_day > 23) or (m_month == 6 and m_day < 22)) {
+    m_zodiac_sign_month = "Gemini";
+  } else if((m_month == 6 and m_day > 23) or (m_month == 7 and m_day < 23)) {
+    m_zodiac_sign_month = "Cancer";
+  } else if((m_month == 7 and m_day > 24) or (m_month == 8 and m_day < 24)) {
+    m_zodiac_sign_month = "Leo";
+  } else if((m_month == 8 and m_day > 25) or (m_month == 9 and m_day < 23)) {
+    m_zodiac_sign_month = "Virgo";
+  } else if((m_month == 9 and m_day > 24) or (m_month == 10 and m_day < 24)) {
+    m_zodiac_sign_month = "Libra";
+  } else if((m_month == 10 and m_day > 25) or (m_month == 11 and m_day < 23)) {
+    m_zodiac_sign_month = "Scorpio";
+  } else if((m_month == 11 and m_day > 24) or (m_month == 12 and m_day < 22)) {
+    m_zodiac_sign_month = "Sagittarius";
+  }
+}
